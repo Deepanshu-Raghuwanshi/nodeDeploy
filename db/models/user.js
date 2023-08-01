be87446c8d14c4567996ff8f7732b3eff8fc49ac
Deepanshu-Raghuwanshi <dipanshuraghuwanshi@gmail.com>
2023-08-01 21:53:58 +0530

    Express

---
+++ db/models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const usersSchema = new Schema({
+  name: String, // String is shorthand for {type: String}
+  email: String,
+  about: String,
+  image: String,
+  username: {
+    type: String,
+    unique: true,
+    index: true,
+  },
+  password: String,
+  date: { type: Date, default: Date.now },
+});
+const userModel = mongoose.model("user", usersSchema);
+module.exports = userModel;
